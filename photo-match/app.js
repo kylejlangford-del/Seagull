9bf951b0181ee2b4c6ea7b8816ee4f0546653f27
kylejlangford-del <kylej.langford@gmail.com>
2026-09-25 01:28:54 +0200

    DIAGNOSTIC: fix duplicated ambient light declaration

--- photo-match/app.js
+++ photo-match/app.js
@@ -207,9 +207,6 @@ function initScene() {
   const diagAmbient = new THREE.AmbientLight(0xffffff, 5.0);
   scene.add(diagAmbient);
 
-  const diagAmbient = new THREE.AmbientLight(0xffffff, 5.0);
-  scene.add(diagAmbient);
-
   createWaterGuide();
 
   window.addEventListener('resize', onResize);
